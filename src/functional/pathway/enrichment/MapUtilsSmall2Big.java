f4bf8da4c4a2b8a372c2b0b2d8ad23a0c93ca5f9
gatechatl <gatechatl@gmail.com>
2020-08-08 14:44:47 -0500

    Updated DRPPM from new machine

--- src/functional/pathway/enrichment/MapUtilsSmall2Big.java
+++ src/functional/pathway/enrichment/MapUtilsSmall2Big.java
@@ -69,58 +69,6 @@ public class MapUtilsSmall2Big {
 			return null;
 		}
 
-		public <T extends Comparable<? super T>> Comparator<T> reverseOrder() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public <T extends Comparable<? super T>> Comparator<T> naturalOrder() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public <T> Comparator<T> nullsFirst(
-				Comparator<? super T> comparator) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public <T> Comparator<T> nullsLast(
-				Comparator<? super T> comparator) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public <T, U> Comparator<T> comparing(
-				Function<? super T, ? extends U> keyExtractor,
-				Comparator<? super U> keyComparator) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public <T, U extends Comparable<? super U>> Comparator<T> comparing(
-				Function<? super T, ? extends U> keyExtractor) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public <T> Comparator<T> comparingInt(
-				ToIntFunction<? super T> keyExtractor) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public <T> Comparator<T> comparingLong(
-				ToLongFunction<? super T> keyExtractor) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public <T> Comparator<T> comparingDouble(
-				ToDoubleFunction<? super T> keyExtractor) {
-			// TODO Auto-generated method stub
-			return null;
-		}
     } );
 
     Map<K, V> result = new LinkedHashMap<K, V>();
