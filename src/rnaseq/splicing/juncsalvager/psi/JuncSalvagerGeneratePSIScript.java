a80d63752b1a886be22bffea50512cebbadbb93e
gatechatl <gatechatl@gmail.com>
2020-08-20 21:50:11 -0500

    Add 3' 5' alt splice to the pipeline script generator.

--- src/rnaseq/splicing/juncsalvager/psi/JuncSalvagerGeneratePSIScript.java
+++ src/rnaseq/splicing/juncsalvager/psi/JuncSalvagerGeneratePSIScript.java
@@ -42,6 +42,7 @@ public class JuncSalvagerGeneratePSIScript {
 				File f = new File(str);
 				if (f.exists()) {
 					out.write("drppm -JuncSalvagerExonSkippingPSI " + str + " " + gtfFile + " 5 " + outputFolder + "/" + f.getName() + ".pso.txt" + " " + outputFolder + "/" + f.getName() + ".psi.txt" + "\n");
+					out.write("drppm -JuncSalvager5prime3primeUsagePSI " + str + " " + gtfFile + " 0 " + outputFolder + "/" + f.getName() + ".5to3AltSplice.psi.txt" + "\n");
 					//System.out.println("drppm -JuncSalvagerExonSkippingPSI " + fileLst + " " + gtfFile + " " + outputFolder + "/" + f.getName() + ".psi.txt");
 				} else {
 					System.out.println("Warning! File doesn't exist: " + f.getPath());
