094b56924b7e6be3185aafa2e4ca7c737efe81d8
gatechatl <gatechatl@gmail.com>
2021-02-10 19:41:14 -0500

    drppm -CombineHTSEQResultRaw: Will skip file with 0 byte when combining raw htseq file. drppm -GTFAnnotateNumExon: will add biotype to the geneName.

--- src/ProgramDescriptions.java
+++ src/ProgramDescriptions.java
@@ -2808,6 +2808,6 @@ public class ProgramDescriptions {
 		return result;
 	}	
 
-	public static String VERSION = "2021-0131A";
+	public static String VERSION = "2021-0210A";
 	
 }
--- src/rnaseq/tools/genelengthanalysis/GTFAnnotateNumExon.java
+++ src/rnaseq/tools/genelengthanalysis/GTFAnnotateNumExon.java
@@ -59,63 +59,75 @@ public class GTFAnnotateNumExon {
 			FileWriter fwriter_exon_len = new FileWriter(exon_length_output);
 			BufferedWriter out_exon_len = new BufferedWriter(fwriter_exon_len);
 			
+			HashMap geneName2biotype = new HashMap();
 			
 			FileInputStream fstream = new FileInputStream(inputFile);
 			DataInputStream din = new DataInputStream(fstream);
 			BufferedReader in = new BufferedReader(new InputStreamReader(din));					
 			while (in.ready()) {
 				String str = in.readLine().trim();
-				String[] split = str.split("\t");
-				String geneType = split[2];
-				int length = new Integer(split[4]) - new Integer(split[3]);
-				String meta = split[8];
-				String gene_id = GTFFile.grabMeta(meta, "gene_id");
-				String gene_name = GTFFile.grabMeta(meta, "gene_name");
-				String transcript_id = GTFFile.grabMeta(meta, "transcript_id");
-				transcript2GeneName.put(transcript_id, gene_name);
-				transcript2GeneID.put(transcript_id, gene_id);
-				if (geneName2transcript.containsKey(gene_name)) {
-					LinkedList list = (LinkedList)geneName2transcript.get(gene_name);
-					list.add(transcript_id);
-					geneName2transcript.put(gene_name, list);
-				} else {
-					LinkedList list = new LinkedList();
-					list.add(transcript_id);
-					geneName2transcript.put(gene_name, list);
-				}
-				
-				if (geneID2transcript.containsKey(gene_id)) {
-					LinkedList list = (LinkedList)geneID2transcript.get(gene_id);
-					list.add(transcript_id);
-					geneID2transcript.put(gene_id, list);
-				} else {
-					LinkedList list = new LinkedList();
-					list.add(transcript_id);
-					geneID2transcript.put(gene_id, list);
-				}
-				if (geneType.equals("exon")) {
-					exon_length.put(transcript_id + "_" + exon_id, length);
-					exon_id++;
+				if (!str.substring(0, 2).equals("##")) {
+					String[] split = str.split("\t");
+					String geneType = split[2];
+					int length = new Integer(split[4]) - new Integer(split[3]);
+					String meta = split[8];
+					String gene_id = GTFFile.grabMeta(meta, "gene_id");
+					String gene_name = GTFFile.grabMeta(meta, "gene_name");
+					String transcript_id = GTFFile.grabMeta(meta, "transcript_id");
 					
-					if (transcript_length.containsKey(transcript_id)) {
-						int len = (Integer)transcript_length.get(transcript_id);
-						len = len + length;
-						transcript_length.put(transcript_id, len);
+
+					//String geneID = ((String)GTFFile.transcript_clean2gene.get(split[0].replaceAll("\"",  "").split("\\.")[0]));
+					String biotype = GTFFile.grabMeta(meta, "gene_type");
+					if (biotype.equals("")) {
+						biotype = GTFFile.grabMeta(meta, "biotype");
+					}
+					geneName2biotype.put(gene_name, biotype);
+					
+					transcript2GeneName.put(transcript_id, gene_name);
+					transcript2GeneID.put(transcript_id, gene_id);
+					if (geneName2transcript.containsKey(gene_name)) {
+						LinkedList list = (LinkedList)geneName2transcript.get(gene_name);
+						list.add(transcript_id);
+						geneName2transcript.put(gene_name, list);
 					} else {
-						transcript_length.put(transcript_id, length);
+						LinkedList list = new LinkedList();
+						list.add(transcript_id);
+						geneName2transcript.put(gene_name, list);
 					}
 					
-					if (exonInGene.containsKey(gene_name)) {
-						int count = (Integer)exonInGene.get(gene_name);
-						count = count + 1;
-						exonInGene.put(gene_name, count);
+					if (geneID2transcript.containsKey(gene_id)) {
+						LinkedList list = (LinkedList)geneID2transcript.get(gene_id);
+						list.add(transcript_id);
+						geneID2transcript.put(gene_id, list);
 					} else {
-						exonInGene.put(gene_name, 1);
+						LinkedList list = new LinkedList();
+						list.add(transcript_id);
+						geneID2transcript.put(gene_id, list);
 					}
 					
-					
-				}				
-				
+					if (geneType.equals("exon")) {
+						exon_length.put(transcript_id + "_" + exon_id, length);
+						exon_id++;
+						
+						if (transcript_length.containsKey(transcript_id)) {
+							int len = (Integer)transcript_length.get(transcript_id);
+							len = len + length;
+							transcript_length.put(transcript_id, len);
+						} else {
+							transcript_length.put(transcript_id, length);
+						}
+						
+						if (exonInGene.containsKey(gene_name)) {
+							int count = (Integer)exonInGene.get(gene_name);
+							count = count + 1;
+							exonInGene.put(gene_name, count);
+						} else {
+							exonInGene.put(gene_name, 1);
+						}
+						
+						
+					}				
+				}
 			}
 			in.close();
 			
@@ -137,7 +149,9 @@ public class GTFAnnotateNumExon {
 				Iterator itr2 = list.iterator();
 				while (itr2.hasNext()) {
 					String transcript_id = (String)itr2.next();
-					total += (Integer)transcript_length.get(transcript_id);
+					if (transcript_length.containsKey(transcript_id)) {
+						total += (Integer)transcript_length.get(transcript_id);
+					}
 				}
 				out_geneID.write(geneID + "\t" + (total / list.size()) + "\n");
 			}
@@ -152,7 +166,9 @@ public class GTFAnnotateNumExon {
 				Iterator itr2 = list.iterator();
 				while (itr2.hasNext()) {
 					String transcript_id = (String)itr2.next();
-					total += (Integer)transcript_length.get(transcript_id);
+					if (transcript_length.containsKey(transcript_id)) {
+						total += (Integer)transcript_length.get(transcript_id);
+					}
 				}
 				out_geneName.write(geneName + "\t" + (total / list.size()) + "\n");
 			}
@@ -164,8 +180,9 @@ public class GTFAnnotateNumExon {
 			while (itr.hasNext()) {
 				String geneName = (String)itr.next();
 				int total = (Integer)exonInGene.get(geneName);
-				
-				out_geneName_exon.write(geneName + "\t" + total + "\n");
+				if (exonInGene.containsKey(geneName)) {
+					out_geneName_exon.write(geneName + "\t" + total + "\t" + geneName2biotype.get(geneName) + "\n");
+				}
 			}
 			out_geneName_exon.close();
 			
@@ -173,8 +190,10 @@ public class GTFAnnotateNumExon {
 			itr = exon_length.keySet().iterator();
 			while (itr.hasNext()) {
 				String exon_id_key = (String)itr.next();
-				int length = (Integer)exon_length.get(exon_id_key);
-				out_exon_len.write(exon_id_key + "\t" + length + "\n");
+				if (exon_length.containsKey(exon_id_key)) {
+					int length = (Integer)exon_length.get(exon_id_key);					
+					out_exon_len.write(exon_id_key + "\t" + length + "\n");
+				}
 			}
 			out_exon_len.close();
 		} catch (Exception e) {
