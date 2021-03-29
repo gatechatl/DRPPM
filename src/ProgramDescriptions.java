e80546ca743fb7465285c3e7d450eb7912b299ae
gatechatl <gatechatl@gmail.com>
2021-03-29 07:57:47 -0400

    Extract the QC metric from the STAR 2.7.1a output. Check whether the file exists.

--- src/ProgramDescriptions.java
+++ src/ProgramDescriptions.java
@@ -2851,7 +2851,5 @@ public class ProgramDescriptions {
 		}
 		return result;
 	}	
-
-	public static String VERSION = "2021-0329A";
-	
+	public static String VERSION = "2021-0329B";	
 }
--- src/rnaseq/tools/qc/star_2_7_1a/ExtractQCMetricsSTAR271a.java
+++ src/rnaseq/tools/qc/star_2_7_1a/ExtractQCMetricsSTAR271a.java
@@ -3,6 +3,7 @@ package rnaseq.tools.qc.star_2_7_1a;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
@@ -10,13 +11,18 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+/**
+ * Extracting the QC output from the STAR *final.out files
+ * @author gatechatl
+ *
+ */
 public class ExtractQCMetricsSTAR271a {
 
 	public static String type() {
 		return "STAR";
 	}
 	public static String description() {
-		return "Extract the QC output from STAR *final.out";
+		return "Extract the QC output from the STAR *final.out files";
 	}
 	public static String parameter_info() {
 		return "[inputFileList] [outputFile]";
@@ -41,48 +47,51 @@ public class ExtractQCMetricsSTAR271a {
 				String[] split = str.split("\t");
 				String sampleName = split[0];
 				String fileName = split[1];
-				FileInputStream fstream2 = new FileInputStream(fileName);
-				DataInputStream din2 = new DataInputStream(fstream2); 
-				BufferedReader in2 = new BufferedReader(new InputStreamReader(din2));
-				in2.readLine();
-				in2.readLine();
-				in2.readLine();
-				in2.readLine();
-				in2.readLine();
-				while (in2.ready()) {
-					String str2 = in2.readLine();
-					str2 = str2.trim();
-					str2 = str2.replaceAll(" ", "").replaceAll("\t", "");
-					String[] split2 = str2.split("\\|");
-					if (split2.length == 2) {
-						if (first) {
-							list.add(split2[0]);
+				File f = new File(fileName);
+				if (f.exists()) {
+					FileInputStream fstream2 = new FileInputStream(fileName);
+					DataInputStream din2 = new DataInputStream(fstream2); 
+					BufferedReader in2 = new BufferedReader(new InputStreamReader(din2));
+					in2.readLine();
+					in2.readLine();
+					in2.readLine();
+					in2.readLine();
+					in2.readLine();
+					while (in2.ready()) {
+						String str2 = in2.readLine();
+						str2 = str2.trim();
+						str2 = str2.replaceAll(" ", "").replaceAll("\t", "");
+						String[] split2 = str2.split("\\|");
+						if (split2.length == 2) {
+							if (first) {
+								list.add(split2[0]);
+							}
+							System.out.println(split2[0] + "\t" + split2[1]);
+							map.put(split2[0], split2[1]);
 						}
-						System.out.println(split2[0] + "\t" + split2[1]);
-						map.put(split2[0], split2[1]);
 					}
-				}
-				in2.close();
-				
-				if (first) {
+					in2.close();
+					
+					if (first) {
+						Iterator itr = list.iterator();
+						while (itr.hasNext()) {
+							String tag = (String)itr.next();
+							String value = (String)map.get(tag);
+							out.write("\t" + tag);
+						}
+						out.write("\n");
+						
+					}
+					first = false;
+					out.write(sampleName);
 					Iterator itr = list.iterator();
 					while (itr.hasNext()) {
 						String tag = (String)itr.next();
 						String value = (String)map.get(tag);
-						out.write("\t" + tag);
+						out.write("\t" + value);
 					}
 					out.write("\n");
-					
 				}
-				first = false;
-				out.write(sampleName);
-				Iterator itr = list.iterator();
-				while (itr.hasNext()) {
-					String tag = (String)itr.next();
-					String value = (String)map.get(tag);
-					out.write("\t" + value);
-				}
-				out.write("\n");
 			}
 			in.close();
 			out.close();
