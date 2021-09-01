ac6064fa1d77ed1c4cbd7688b860cc5cae338d4a
gatechatl <gatechatl@gmail.com>
2021-09-01 16:53:25 -0400

    Updated the default memory restriction to the STAR command to 32gb

--- src/stjude/projects/jinghuizhang/hg38mapping/star/JinghuiZhangSTARMappingFromYawei.java
+++ src/stjude/projects/jinghuizhang/hg38mapping/star/JinghuiZhangSTARMappingFromYawei.java
@@ -98,7 +98,8 @@ public class JinghuiZhangSTARMappingFromYawei {
 		line += "     --outWigType wiggle \\\n";
 		line += "     --outWigStrand Stranded \\\n";
 		line += "     --outWigNorm RPM \\\n";
-		line += "     --limitBAMsortRAM 100672447591\\\n";
+		//line += "     --limitBAMsortRAM 100672447591\\\n";
+		line += "     --limitBAMsortRAM 31744190143\\\n";
 		if (gzip_flag) {
 			line += "     --readFilesCommand zcat\\\n";
 		}
